29943688c8dc2aa3396c6b61c0564ef494a9ed4f
lqp336699 <lqp336699@gmail.com>
2023-10-30 18:20:54 +0800

    00

---
+++ pages/home/home.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="page px22">
+		
+		<ProductCard v-for="item in 2" :key="item"></ProductCard>
+	</view>
+</template>
+
+<script>
+	import ProductCard from '@/components/ProductCard.vue'
+	export default {
+		components:{
+			ProductCard
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
